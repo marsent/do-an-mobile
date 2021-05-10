a1ce1e36a2376e0d4025b64bcace77630395ec6e
thuandhm2k <thuandhm2k@gmail.com>
2021-05-10 20:58:25 +0700

    add listClass

---
+++ Lecture-App/src/components/ClassManagement/listClass.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  KeyboardAvoidingView,
+} from 'react-native';
+export default function listClass() {
+    return(
+        <View style = {styles.container}>
+            <View>
+                <Text>mother fucker</Text>
+            </View>
+            <View>
+
+            </View>
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    container: {
+        flex : 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        
+    },
+    up: {
+
+    },
+    down: {
+
+    }
+});
